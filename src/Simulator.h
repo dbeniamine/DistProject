6265dd771a1ae54fa5ae4d53587b211a9b2c93f2
Rodolphe Lepigre <rlepigre@gmail.com>
2012-12-11 17:20:49 +0100

    forgot one file...

--- src/Simulator.h
+++ src/Simulator.h
@@ -22,17 +22,21 @@ Message_t initMessage(const char* str, int snd, int rcv);
 // Failures are handled internaly.
 void deleteMessage(Message_t msg);
 
+// Copy a message.
+//   msg : the message
+// Failures are handled internaly.
+// Returns a Message_t object.
+Message_t copyMessage(Message_t msg);
+
 //Algorithm run by each process
 //the int is the unique id of the process in [0,NbProcess]
 //the Message_t is either NULL or the message received at the beginning of the turn
 //The Fifo is the list of external event which happens to the node at the
 //beggining of the round
-
 typedef void(*NodesFct_t)(int , Message_t,Fifo );
 
 //A node can use this method to send a message m which must be correcly
 //initialized, return 0 on success
-
 int Send(Message_t m);
 
 
@@ -40,7 +44,6 @@ int Send(Message_t m);
 //Each Node run the functon f
 //The external events are read by the simulator on the standard input
 void LaunchSimulation(int NbRounds, int NbProcess, NodesFct_t f);
-
 
 #endif //__SIMULATOR_H
 
